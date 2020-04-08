34a691e70b643e7eff3b2ca44969ddd1bcd4df7f
eldho <eldhopaul90@gmail.com>
2020-04-08 12:15:20 +0530

    Added query to get customer details by MobNo

---
+++ MeruWebApi/Controllers/CustomerController.cs
@@ -0,0 +1,55 @@
+﻿using MeruWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MeruWebApi.Controllers
+{
+    public class CustomerController : ApiController
+
+       
+    {
+        MeruDbContext db;
+
+        public CustomerController()
+        {
+            db = new MeruDbContext();
+        }
+
+        [Route("GetCustomerByMobNo")]
+        public IHttpActionResult GetCustomerByMobNo(string MobNo)
+        {
+            try
+            {
+                var CustomerDetails = (from cust in db.Customers
+                                       join sbarea in db.SubAreas
+                                       on cust.SubAreaId equals sbarea.SubAreaId
+                                       join area in db.Areas
+                                       on sbarea.AreaId equals area.AreaId
+                                       join cty in db.Cities
+                                       on area.CityId equals cty.CityId
+                                       where cust.CustMobNo == MobNo
+                                       select new CustomerVM
+                                       {
+                                           CustName = cust.CustName,
+                                           Address = cust.Address,
+                                           CustMobNo = cust.CustMobNo,
+                                           SubAreaName = sbarea.SubAreaName,
+                                           AreaName = area.AreaName,
+                                           CityName = cty.CityName,
+                                       }).Single();
+
+                return Ok(CustomerDetails);
+            }
+            catch (Exception ex)
+            {
+
+                var errMsg = ex.Message;
+                return InternalServerError();
+            }
+        }
+    }
+}
---
+++ MeruWebApi/Models/CustomerVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MeruWebApi.Models
+{
+    public class CustomerVM
+    {
+
+        public long CustId { get; set; }
+        public string CustName { get; set; }
+        public string CustMobNo { get; set; }
+        public string Address { get; set; }
+        public Nullable<long> SubAreaId { get; set; }
+        public string SubAreaName { get; set; }
+        public string AreaName { get; set; }
+        public string CityName { get; set; }
+
+
+    }
+}
